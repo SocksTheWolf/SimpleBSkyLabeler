89d1b3818677a1ca1b11248a21e8275f2ece2fae
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-01-08 16:13:36 -0800

    Add partial index to SQL query
    this is very redundant, and won't do a whole lot more as we don't have revocations, but w/e
    
    Signed-off-by: SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>

--- setup.sql
+++ setup.sql
@@ -4,3 +4,5 @@ CREATE TABLE IF NOT EXISTS labeled (
   time TIMESTAMP DEFAULT (STRFTIME('%Y-%m-%dT%H:%M:%fZ', 'NOW', 'localtime')) NOT NULL, 
   neg INTEGER DEFAULT 0 NOT NULL 
 );
+CREATE INDEX IF NOT EXISTS idx_labeled_non_removed ON labeled(neg) WHERE neg == 0;
+pragma optimize
